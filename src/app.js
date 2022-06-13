8610a30a3a1947fdd12e05b7d5ab00f1771acc0f
affan-11 <affanarif4@gmail.com>
2022-06-13 16:40:33 +0100

    finished

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const yargs = require("yargs");
+const { sequelize } = require("./db/connection");
+const { addMovie, listMovies, updateMovie, deleteMovie } = require("./movie/functions");
+
+const app = async (yargsObj) => {
+    try {
+        await sequelize.sync();
+        if (yargsObj.add) {
+            //take movie key value pairs from yargsObj, send them to an add function and return movie
+            await addMovie({title: yargsObj.title, actor: yargsObj.actor});
+        } else if (yargsObj.list) {
+            //list all movies from database
+            await listMovies();
+        } else if (yargsObj.update) {
+            //take filter and update key value pairs form yargsObj, send them to update function and return success/failure message
+            await updateMovie({ actor: yargsObj.actor }, { title: yargsObj.title });
+        } else if (yargsObj.delete) {
+            //take filter k/v pair from yargsObj and sennd them to delete fucntion, return success/failure message
+            await deleteMovie({ title: yargsObj.title });
+        } else {
+            console.log("incorrect command")
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app(yargs.argv);
